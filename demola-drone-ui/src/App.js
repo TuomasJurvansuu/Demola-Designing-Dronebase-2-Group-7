589c13f2b74d05ec4a2b65cbd0b49c20a3be01ed
Tuomas Jurvansuu <T9jutu01@students.oamk.fi>
2025-04-01 13:50:05 +0300

    Status vaihdettu logs navbariin

--- demola-drone-ui/src/App.js
+++ demola-drone-ui/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import RoutePlanner from "./pages/RoutePlanner";
-import Status from "./pages/Status";
 import ManualControl from "./pages/ManualControl";
 import Settings from "./pages/Settings";
 import "./styles/global.css";
 import "leaflet/dist/leaflet.css";
+import Log from "./pages/Log";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/route-planner" element={<RoutePlanner />} /> 
-        <Route path="/status" element={<Status />} />
+        <Route path="/Log" element={<Log />} />
         <Route path="/manual" element={<ManualControl />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
